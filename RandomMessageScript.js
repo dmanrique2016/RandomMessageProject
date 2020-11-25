1886674742c7d8913319ada9d3965881cbc384dc
manriqueperezdavid <manriqueperezdavid@gmail.com>
2020-11-25 16:11:20 +0100

    Create new random variable's to be displayed

--- RandomMessageScript.js
+++ RandomMessageScript.js
@@ -2,3 +2,7 @@
 const times = ['Tomorrow','Today','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 const meals = ['breakfast','lunch','dinner','snack']
 const food = ['Scrambled eggs','Cereals','Corn flakes','Omellete','Salad','Soup','Pasta','Fried Chicken','Meatballs','Cauliflower','Tuna']
+
+let random_time = times[Math.floor(Math.random()*times.length)]
+let random_meal = meals[Math.floor(Math.random()*meals.length)]
+let random_food = food[Math.floor(Math.random()*food.length)]
