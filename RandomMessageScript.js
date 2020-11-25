5b4e948b5aa940bc6ee00b2214660c0fa7602e1a
manriqueperezdavid <manriqueperezdavid@gmail.com>
2020-11-25 16:02:40 +0100

    Create arrays to be randomized

---
+++ RandomMessageScript.js
@@ -0,0 +1,4 @@
+// We create the arrays that will include the random words to be choosen.
+const times = ['Tomorrow','Today','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+const meals = ['breakfast','lunch','dinner','snack']
+const food = ['Scrambled eggs','Cereals','Corn flakes','Omellete','Salad','Soup','Pasta','Fried Chicken','Meatballs','Cauliflower','Tuna']
